a279094a20ed138f1fba223e8dd00597ece05840
MaxDeb1 <deb1maxime@gmail.com>
2023-06-16 11:53:59 +0200

    MySQL Route Controller Structure

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(8080, () => {
   console.log("Connected to backend!");
